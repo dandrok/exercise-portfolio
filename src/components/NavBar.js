1c566b7e6205161cb1710761727be066fb5cd23e
dandrok <patryk.stusik@gmail.com>
2021-08-08 20:19:37 +0200

    README.md

---
+++ src/components/NavBar.js
@@ -0,0 +1,16 @@
+import styles from './NavBar.module.scss'
+
+const NavBar = () => {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>MY_NAME</li>
+          <li>MENU</li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
+
+export default NavBar
